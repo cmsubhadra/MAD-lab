6459d422c3ae2ef22472a47e6dbc930e178b7829
cmsubhadra <74703174+cmsubhadra@users.noreply.github.com>
2022-02-10 23:05:32 +0530

    Add files via upload

---
+++ Checkbox/checkbox.java
@@ -0,0 +1,50 @@
+package com.example.myapplication13;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkBox1:
+                if (checked) {
+                    displaymessage("selected BCA");
+                }
+                else {
+                    displaymessage("removed BCA");
+                }
+                break;
+            case R.id.checkBox2:
+                if (checked) {
+                    displaymessage("selected MCA");
+                }
+                else {
+                    displaymessage("removed MCA");
+                }
+
+                break;
+
+        }
+
+
+
+    }
+    public void displaymessage (String s)
+    {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
